0cda76ea98d9018eae5ad6573072d8770b2dc200
honeynagrikar <honey12nagrikar@gmail.com>
2020-06-22 12:18:55 +0000

    Info

---
+++ fourth.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter your Name :"
+read name
+
+echo
+
+echo "You're intrested in IT (y/n) ? "
+read like
+
+if [ $like == y ]
+then
+echo "you're doing great and achive big in future"
+else
+echo "Please try IT nothing is good than it "
+fi
+
